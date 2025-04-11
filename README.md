eb47490f396096cb8d2fe837e75ba23c8ad77ecd
LeahJB <ljb@niva.no>
2025-04-11 10:15:06 +0000

    Tidy repository

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # doc-retention
+
+Repository for exploring simple models for predicting DOC transmission across (i.e. the proportion of DOC out / in) using water transit time. The main approaches included are exponential decay and a steady state Vollenweider approach. For both of these, we compare models with constant decay constants to models where the decay constant changes with time as k = a t^b.
+
+The **'data'** folder contains the following data, which form the basis of the analysis:
+
+1. catalan_2016_doc_data.xlsx: Data from the supplementary information to Catalan et al. (2016)
+2. evans_2017_doc_retention_cleaned.csv: Data from the supplementary information to Evans et al. (2017). This data also includes one data point for retention in Lake Langtjern, Norway (data from NIVA).
+3. review_instream_doc_retention_summary.csv: Data on river and stream DOC transmission gathered for this study
+4. combined_data_2025-04.csv: A compilation of the above 3 data files, including only field data.
+
+Data exploration and analysis is then done using python within a series of Jupyter Notebooks. Main notebooks are:
+
+* 01_combine_all_datasets.ipynb: Combine the first three of the above datasets to make dataset 4.
+* 02_fit_models_combined_datasets.ipynb: Fit exponential and Vollenweider models with constand and time-varying decay constants to the the combined dataset (4) and explore differences in model performance.
+* 03_k_vs_tau_results: Visualise differences in k vs t from fitting different models to different datasets
+* 04_model_comparison_catalan_data.ipynb: Fit exponential models with constan and time-varying decay constants to the the Catalan et al. data (dataset 1) only. Fit models to field and bioassay data separately and compare coefficients and fits.
+* 05_model_comparison_evans_data.ipynb: Fit models to the Evans et al. data (dataset 2) only. Compare Vollenweider and exponential decay approaches.
+* 06_explore_catalan_vs_random_data.ipynb: Exploratoration of the Catalan et al. data. Comparison of k vs t regression results generated using real vs random data.
+* check_site_overlap.ipynb: Exploring overlap in lake sites between datasets 1 and 2
+
+Functions for model fitting and analysis used across notebooks are in doc_retention_utils.py
+
+**References**
+- Catalán, N., Marcé, R., Kothawala, D. N., & Tranvik, L. J. (2016). Organic carbon decomposition rates controlled by water retention time across inland waters. Nature Geoscience, 9(7), 501-504
+- Evans, C. D., Futter, M. N., Moldan, F., Valinia, S., Frogbrook, Z., & Kothawala, D. N. (2017). Variability in organic carbon reactivity across lake residence time and trophic gradients. Nature Geoscience, 10(11), 832-835 
--- doc_retention_utils.py
+++ doc_retention_utils.py
@@ -1,7 +1,7 @@
 import numpy as np
+import lmfit
 from lmfit import Model
 from scipy.stats import chi2
-from lmfit import Model
 from sklearn.metrics import r2_score, mean_absolute_error
 
 
@@ -10,9 +10,9 @@ def transmission_k_constant(tau, k=0.5):
     return np.exp(-k * tau)
 
 
-def transmission_k_as_func_of_tau(tau, p=0.5, m=-0.5):
+def transmission_k_as_func_of_tau(tau, a=0.5, b=-0.5):
     """Model assuming exponential decay with k=f(t)"""
-    return np.exp(-p * (tau ** (m + 1)))
+    return np.exp(-a * (tau ** (b + 1)))
 
 
 def transmission_sigma_constant(tau, sigma=0.5):
@@ -20,9 +20,9 @@ def transmission_sigma_constant(tau, sigma=0.5):
     return 1 / (1 + (sigma * tau))
 
 
-def transmission_sigma_as_func_of_tau(tau, k=0.5, m=-0.5):
+def transmission_sigma_as_func_of_tau(tau, a=0.5, b=-0.5):
     """Vollenweider model, sigma=f(tau)"""
-    return 1 / (1 + (k * (tau ** (1 + m))))
+    return 1 / (1 + (a * (tau ** (1 + b))))
 
 
 def fit_model(func, df, tau_col, trans_col):
@@ -115,13 +115,13 @@ def rs_mae(s1_obs, s2_pred, model_name):
 def calculate_bic(obs_s, pred_s, num_params):
     """
     Calculate Bayesian Information Criterion (BIC).
-    
+
     Parameters:
     df (pd.DataFrame): DataFrame containing the data.
     obs_col (str): Column name for observations.
     pred_col (str): Column name for predictions.
     num_params (int): Number of parameters in the model.
-    
+
     Returns:
     float: BIC value.
     """
@@ -129,3 +129,67 @@ def calculate_bic(obs_s, pred_s, num_params):
     residual_sum_of_squares = np.sum((obs_s - pred_s) ** 2)
     bic = n * np.log(residual_sum_of_squares / n) + num_params * np.log(n)
     print(bic)
+
+
+def st_dev_residuals(model_obj):
+    # Calculate residual standard deviation
+    residuals = model_obj.residual
+    residual_std = np.std(residuals)
+    print("Standard deviation of residuals:", np.round(residual_std, 3))
+    return residual_std
+
+
+def generate_prediction_intervals(model_obj, x, n_samples=1000, add_residual_error=True):
+    """
+    To do: generalise to remove hard-coding of param names
+    """
+    params = model_obj.params
+
+    if len(params) < 2:
+        # Handle single parameter case
+        y_pred = transmission_sigma_constant(x, **params.valuesdict())
+
+        k = params['sigma'].value
+        k_stderr = params['sigma'].stderr
+        if k_stderr is None:
+            raise ValueError("Standard error for parameter 'k' is not available.")
+
+        # Create confidence intervals manually
+        # Assuming a normal distribution (mean=0, s.d.=1), 95% of data are between +/-1.96
+        ci_k_l95 = k - 1.96 * k_stderr
+        ci_k_u95 = k + 1.96 * k_stderr
+
+        y_samples = np.zeros((n_samples, len(x)))
+        for i in range(n_samples):
+            k_sample = np.random.uniform(ci_k_l95, ci_k_u95)
+            y_samples[i, :] = transmission_sigma_constant(x, sigma=k_sample)
+
+        y_upper = np.max(y_samples, axis=0)
+        y_lower = np.min(y_samples, axis=0)
+    else:
+        # Handle multiple parameter case
+        y_pred = transmission_sigma_as_func_of_tau(x, **params.valuesdict())
+        ci = lmfit.conf_interval(model_obj, result=model_obj)
+        ci_k_l95 = ci['k'][1][1]
+        ci_k_u95 = ci['k'][-2][1]
+        ci_m_l95 = ci['m'][1][1]
+        ci_m_u95 = ci['m'][-2][1]
+
+        y_samples = np.zeros((n_samples, len(x)))
+        for i in range(n_samples):
+            k_sample = np.random.uniform(ci_k_l95, ci_k_u95)
+            m_sample = np.random.uniform(ci_m_l95, ci_m_u95)
+            y_samples[i, :] = transmission_sigma_as_func_of_tau(x, k=k_sample, m=m_sample)
+
+        y_upper = np.max(y_samples, axis=0)
+        y_lower = np.min(y_samples, axis=0)
+
+    if add_residual_error:
+        # Calculate residual standard deviation
+        residual_std = st_dev_residuals(model_obj)
+
+        # Adjust intervals to include 95% CIs on residual error
+        y_upper += 1.96 * residual_std
+        y_lower -= 1.96 * residual_std
+
+    return y_pred, y_upper, y_lower
